cf8a1c3ba0d4533f01d2fd24653aec3e99f4b2c4
Mory <Moryforcola@outlook.com>
2023-08-10 10:21:25 +0800

    start

---
+++ src/main/java/com/example/tododesign/TodoDesignApplication.java
@@ -0,0 +1,13 @@
+package com.example.tododesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoDesignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/generator/CodeGenerator.java
@@ -0,0 +1,9 @@
+package com.example.generator;
+
+/**
+ * @author Mory
+ * &date  2023/8/10 10:20
+ * &introduce 烽火兴旺，凡我喵喵教，喵喵喵！
+ */
+public class CodeGenerator {
+}
